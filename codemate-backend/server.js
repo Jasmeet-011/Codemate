3ebe66b953e62c7f721c19151a6ea01c0a3dc1fb
Jasmeet-011 <jasmeetsingh5003@gmail.com>
2025-01-01 17:51:49 -0500

    changed port in backend

--- codemate-backend/server.js
+++ codemate-backend/server.js
@@ -91,6 +91,6 @@ app.get("/api/history", async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
